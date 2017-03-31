1d73b71c50e4bf5ae30212c531c13ccf37e08321
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-03-31 14:48:36 -0700

    create for statement to cycle through pingsArray in if else statement.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 // business logic
-var pingArray = ["pingpong", "ping", "pong"]
 var numbersTest = function(userInput) {
-  pingarray.forEach(function(ping){
+  var pingsArray = ["ping", "pong"];
+  for (var index = 0; index < pingsArray.length; index +=1) {
     if (userInput % 15 === 0) {
-      return pingArray[0];
+      return pingsArray.join('');
     } else if (userInput % 3 == 0) {
-      return pingArray[1]
+      return pingsArray[0]
     } else if (userInput % 5 === 0) {
-      return pingArray[2]
+      return pingsArray[1]
     } else {
       return userInput;
     };
   };
-});
+};
 //user interface logic
 $(document).ready(function(){
   $("form#number-form").submit(function(event){
