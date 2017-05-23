4075f9a2f994ed2e301c57675d7454744d96d985
Niklas Long and Nicole Santamaria <niklas@>
2017-05-23 09:42:47 -0700

    add humidity to ping pong game

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -6,7 +6,10 @@ $(document).ready(function() {
     $('#location').val("");
     $('.showWeather').text("The city you have chosen is " + city + ".");
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-      console.log(response);
+
+      var weather = (response.main.humidity);
+      console.log(weather);
+       $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
     });
   });
 });
