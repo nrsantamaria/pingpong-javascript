4580f4ec98eb42ebe4d9909c82c0f9f852d799f7
Niklas Long and Nicole Santamaria <niklas@>
2017-05-23 11:08:36 -0700

    refactor weather js

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,15 +1,14 @@
-var apiKey = "eab4ef10a52f81ef6385eebafe9cd1bd";
+var Weather = require('./../js/weather.js').weatherModule;
+
+var displayHumidity = function(city, humidityData) {
+  $('.showWeather').text("The humidity in " + city + " is " + humidityData + "%");
+}
 
 $(document).ready(function() {
+  var currentWeatherObject = new Weather();
   $('#weather-location').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $('.showWeather').text("The city you have chosen is " + city + ".");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-
-      var weather = (response.main.humidity);
-      console.log(weather);
-       $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-    });
+    currentWeatherObject.getWeather(city, displayHumidity);
   });
 });
